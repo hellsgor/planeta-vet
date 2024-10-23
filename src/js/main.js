348a16a619423a23472c7ee6535304496b580067
Igor Storchevoy <hellsgor@gmail.com>
2024-10-23 16:55:45 +0400

    Feat: cleaned project

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,2 @@
 import '../../node_modules/modern-normalize/modern-normalize.css';
 import '../scss/style.scss';
-import { setupCounter } from './utils/counter';
-
-setupCounter(document.getElementById('counter'));
