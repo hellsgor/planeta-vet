193cfd03dbb03ae39307cf031cdecd044e7ca6db
Igor Storchevoy <hellsgor@gmail.com>
2024-10-25 00:36:34 +0400

    Update[footer.hbs]: added data from context

--- src/stores/context.js
+++ src/stores/context.js
@@ -1,1 +1,16 @@
-export const context = {};
+export const context = {
+  footer: {
+    navigation: [
+      { href: '/ambassador.html', text: 'Амбассадор' },
+      { href: '/partner-clinic.html', text: 'Клиника-партнёр' },
+      { href: '/doctor.html', text: 'Врач клиники' },
+      { href: '/advantages.html', text: 'Преимущества' },
+      { href: '/contacts.html', text: 'Контакты' },
+    ],
+    contacts: [
+      { href: 'tel:79681876936', text: '+7 968 187-69-36' },
+      { href: 'mailto:okp@planetvet.ru', text: 'okp@planetvet.ru' },
+    ],
+    social: [{ href: 'javascript:void(0)', text: 'Написать в Telegram', icon: '' }],
+  },
+};
