1e01eb6634e28fe332dc4660c5d78fba3c03b181
Igor Storchevoy <hellsgor@gmail.com>
2024-10-25 17:55:43 +0400

    Feat[path-to-success]: added message text content transfer

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,11 @@
 import '../../node_modules/modern-normalize/modern-normalize.css';
 import '../scss/style.scss';
+
+// import { ResolutionChecker } from './utils/ResolutionChecker';
+import { initPathToSuccess } from './components/path-to-success';
+
+// export const resolutionChecker = new ResolutionChecker();
+
+document.addEventListener('DOMContentLoaded', () => {
+  initPathToSuccess();
+});
