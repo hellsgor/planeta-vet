2eb21fad37df95773ed18c04f78cd90bd29192fd
Igor Storchevoy <hellsgor@gmail.com>
2024-10-29 00:58:57 +0400

    Feat: added block data

--- src/stores/home.js
+++ src/stores/home.js
@@ -1,1 +1,20 @@
-export const home = {};
+export const home = {
+  startInNumbers: {
+    title: 'Старт в цифрах',
+    cards: [
+      { title: 'от 600 т. р.', subtitle: 'Минимальная стоимость запуска', iconId: 'rocket' },
+      { title: '2 недели', subtitle: 'Средний срок запуска', iconId: 'hourglass' },
+      { title: '6 месяцев', subtitle: 'Средний срок возврата инвестиций', iconId: 'return-of-investment' },
+      { title: '190 т. р.', subtitle: 'Паушальный взнос', iconId: 'contribution' },
+      {
+        title: 'Откройте свою ветеринарную клинику с нами!',
+        mod: 'big',
+        buttonText: 'Получить бизнес-план',
+        scrollToId: 'path-to-success',
+        imagePath: '../assets/images/raccoon.webp',
+      },
+      { title: '7,1 млн ₽', subtitle: 'Средняя выручка', iconId: 'money-bag' },
+      { title: '+ 600 000 т. р.', subtitle: 'Чистая прибыль в рамках месяца', iconId: 'cash-register' },
+    ],
+  },
+};
