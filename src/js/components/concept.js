bf15b7ae4bc9d974f18aa6e030cc816d5d3ae869
Semion Mudrov <mudrsemen@ya.ru>
2024-10-30 14:31:09 +0300

    Update: updated services info ouput for tablets

--- src/js/components/concept.js
+++ src/js/components/concept.js
@@ -1,18 +1,29 @@
+import { resolutionChecker } from '../main';
+
 export const showServicesInfo = function () {
   const lis = document.querySelectorAll('.concept__services li');
 
   lis.forEach((li) => {
     li.addEventListener('click', function () {
-      // Проверяем, есть ли <p> без класса active внутри текущего <li>
-      if (this.querySelector('p:not(.active)')) {
-        // Убираем класс active у всех <p> внутри <li> элементов
-        lis.forEach((item) => item.querySelector('p').classList.remove('active'));
+      if (!resolutionChecker.isTablet()) {
+        // Проверяем, есть ли <p> без класса active внутри текущего <li>
+        if (this.querySelector('p:not(.active)')) {
+          // Убираем класс active у всех <p> внутри <li> элементов
+          lis.forEach((item) => item.querySelector('p').classList.remove('active'));
 
-        // Добавляем класс active только к текущему <p>
-        this.querySelector('p').classList.add('active');
-      } else {
-        // Если кликнули по открытому <p>, убираем класс active
-        this.querySelector('p').classList.remove('active');
+          // Добавляем класс active только к текущему <p>
+          this.querySelector('p').classList.add('active');
+        } else {
+          // Если кликнули по открытому <p>, убираем класс active
+          this.querySelector('p').classList.remove('active');
+        }
+      }
+      if (resolutionChecker.isTablet()) {
+        const modal = document.querySelector('#modal-services-bubble');
+        modal.querySelector('.modal-services-bubble__title').innerHTML = this.firstChild.textContent.trim();
+        modal.querySelector('.modal-services-bubble__text').innerHTML = this.querySelector('p').textContent;
+
+        modal.showPopover();
       }
     });
   });
