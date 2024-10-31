b9b0a951c878183310629932eca593746d4b0d5f
Igor Storchevoy <hellsgor@gmail.com>
2024-10-31 23:30:10 +0400

    Feat[services.js]: added services to store

--- src/stores/services.js
+++ src/stores/services.js
@@ -20,4 +20,155 @@ export const services = {
     ],
     buttonText: 'Стать партнером',
   },
+
+  services: [
+    {
+      name: 'analyzes',
+      heading: 'Анализы',
+      items: [
+        {
+          nameRu: 'Covid-19',
+          name: 'covid-19',
+        },
+        {
+          nameRu: 'Общеклинические исследования',
+          name: 'general-clinical-studies',
+        },
+        {
+          nameRu: 'Иммуногематология',
+          name: 'immunohematology',
+        },
+        {
+          nameRu: 'Гемостазиологические исследования',
+          name: 'hemostasiological-studies',
+        },
+        {
+          nameRu: 'Биохимические исследования',
+          name: 'biochemical-studies',
+        },
+        {
+          nameRu: 'Аминокислоты и&nbsp;органические кислоты',
+          name: 'amino-acids-and-organic-acids',
+        },
+        {
+          nameRu: 'Жирные кислоты',
+          name: 'fatty-acids',
+        },
+        {
+          nameRu: 'Жирные кислоты',
+          name: 'fatty-acids',
+        },
+        {
+          nameRu: 'Гормональные исследования',
+          name: 'hormonal-studies',
+        },
+        {
+          nameRu: 'Онкомаркеры',
+          name: 'oncomarkers',
+        },
+        {
+          nameRu: 'Иммунологические исследования',
+          name: 'immunological-studies',
+        },
+        {
+          nameRu: 'Аллергология',
+          name: 'allergology',
+        },
+        {
+          nameRu: 'Аутоимунные заболевания',
+          name: 'autoimmune-diseases',
+        },
+        {
+          nameRu: 'Анализы для спортсменов',
+          name: 'analyzes-for-athletes',
+        },
+        {
+          nameRu: 'Микроэлементы и&nbsp;лекарственный мониторинг',
+          name: 'trace-elements-and-drug-monitoring',
+        },
+        {
+          nameRu: 'Наркотические и&nbsp;психоактивные вещества',
+          name: 'narcotic-and-psychoactive-substances',
+        },
+        {
+          nameRu: 'Инфекции',
+          name: 'infections',
+        },
+        {
+          nameRu: 'Бактериологические исследования',
+          name: 'bacteriological-studies',
+        },
+        {
+          nameRu: 'Цитология и микроскопия',
+          name: 'cytology-and-microscopy',
+        },
+        {
+          nameRu: 'Гистология',
+          name: 'histology',
+        },
+        {
+          nameRu: 'Иммуногистохимические исследования',
+          name: 'immunogymic-studies',
+        },
+        {
+          nameRu: 'Генетические исследования',
+          name: 'genetic-studies',
+        },
+        {
+          nameRu: 'Цитогенетические исследования',
+          name: 'cytogenetic-studies',
+        },
+        {
+          nameRu: 'Молекулярно-цитогенетические исследования (FISH)',
+          name: 'molecular-cytogenetic-studies-fish',
+        },
+        {
+          nameRu: 'Q тест - тесты для саморазвития',
+          name: 'q-test-tests-for-self-development',
+        },
+        {
+          nameRu: 'Исследования эякулята',
+          name: 'ejaculate-research',
+        },
+        {
+          nameRu: 'Капиллярная кровь',
+          name: 'capillary-blood',
+        },
+        {
+          nameRu: 'Микологические исследования',
+          name: 'mycological-studies',
+        },
+        {
+          nameRu: 'ПЦР',
+          name: 'pcr',
+        },
+      ],
+    },
+    {
+      name: 'complexes',
+      heading: 'Комплексы',
+      items: [
+        {
+          nameRu: 'Комплекс 1',
+          name: 'complex-1',
+        },
+        {
+          nameRu: 'Комплекс 2',
+          name: 'complex-2',
+        },
+        {
+          nameRu: 'Комплекс 3',
+          name: 'complex-3',
+        },
+        {
+          nameRu: 'Комплекс 4',
+          name: 'complex-4',
+        },
+        {
+          nameRu: 'Комплекс 5',
+          name: 'complex-5',
+        },
+      ],
+    },
+  ],
 };
