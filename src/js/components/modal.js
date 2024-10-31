d79bec0326cc7ff931ac70e9be6caf2e5d26174e
Igor Storchevoy <hellsgor@gmail.com>
2024-10-31 20:46:18 +0400

    Feat[modal/forgot]: added modal for restore password

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,17 +1,30 @@
 class Modal {
   $modal = null;
+  $restorePasswordButton = null;
 
   constructor($modal) {
     this.$modal = $modal;
 
+    if (this.$modal.classList.contains('modal_entrance')) {
+      this.$restorePasswordButton = this.$modal.querySelector('.modal-entrance__forgot');
+    }
+
     this.addEvents();
   }
+
   addEvents() {
     this.$modal.addEventListener('toggle', this.handleModalToggle.bind(this));
+
+    this.$restorePasswordButton &&
+      this.$restorePasswordButton.addEventListener('click', this.handleRestoreButtonClick.bind(this, 'modal-forgot'));
   }
 
   handleModalToggle() {
     document.body.classList[`${this.$modal.matches(':popover-open') ? 'add' : 'remove'}`]('popover-opened');
+  }
+
+  handleRestoreButtonClick(popoverId) {
+    document.getElementById(popoverId).showPopover();
   }
 }
 
