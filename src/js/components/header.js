0d98aec75e57cd1c264ee42b085b75985fb760b0
Igor Storchevoy <hellsgor@gmail.com>
2024-11-12 18:12:24 +0400

    Docs[header.js]: added documentation

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,23 +1,45 @@
 import { resolutionChecker } from '../utils/ResolutionChecker';
 import { Debouncer } from '../utils/Debouncer';
 
+/**
+ * Функция для управления поведением шапки сайта (header) и её взаимодействием с секцией hero.
+ * В зависимости от размера экрана и прокрутки страницы она корректирует отступы и внешние отступы.
+ */
 export const header = function () {
+  /**
+   * Элемент шапки сайта.
+   * @type {HTMLElement}
+   */
   const header = document.querySelector('header');
+
+  /**
+   * Элемент секции hero.
+   * @type {HTMLElement}
+   */
   const hero = document.querySelector('.hero');
 
+  /**
+   * Экземпляр дебаунсера для оптимизации событий изменения размера окна.
+   * @type {Debouncer}
+   */
   const debouncer = new Debouncer();
-  const debouncedManagementHeroPaddingTop = debouncer.debounce(managementHeroPaddingTop, 350);
 
+  // Инициализирует отступы для секции hero
   managementHeroPaddingTop();
 
+  // Добавляет обработчики событий для изменения размера окна и прокрутки
   window.addEventListener('resize', () => {
-    debouncedManagementHeroPaddingTop();
+    debouncer.debounce(managementHeroPaddingTop, 350);
   });
 
   window.addEventListener('scroll', () => {
     managementHeaderMarginTopOnScroll();
   });
 
+  /**
+   * Управляет верхним отступом секции hero в зависимости от размера экрана.
+   * Убирает отступ на мобильных устройствах и добавляет отступ, равный высоте шапки, для других разрешений.
+   */
   function managementHeroPaddingTop() {
     if (resolutionChecker.isMobile()) {
       hero.style.removeProperty('padding-top');
@@ -26,6 +48,11 @@ export const header = function () {
     }
   }
 
+  /**
+   * Управляет верхним отступом шапки при прокрутке страницы.
+   * Если страница прокручена на 34px и более, устанавливается margin-top равным 0.
+   * В противном случае отступ удаляется.
+   */
   function managementHeaderMarginTopOnScroll() {
     if (window.scrollY >= 34) {
       header.style.marginTop = '0';
