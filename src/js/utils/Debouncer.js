5411bf8cc35ad3f9d2e990d39e15cb39ecf8b485
Igor Storchevoy <hellsgor@gmail.com>
2024-11-12 17:50:09 +0400

    Update[Debouncer.js]: updated Debouncer

--- src/js/utils/Debouncer.js
+++ src/js/utils/Debouncer.js
@@ -2,17 +2,21 @@
  * @class Debouncer
  * @description Класс для реализации функции debounce, которая ограничивает количество вызовов функции callee в указанный интервал времени.
  */
+
 export class Debouncer {
+  constructor() {
+    this.lastCall = 0; // Инициализация свойства lastCall
+  }
+
   /**
-   * @description Оборачивает функцию, добавляя к ней задержку (debounce).
+   * Оборачивает функцию, добавляя к ней задержку (debounce).
    * @param {Function} callee - Функция, которую необходимо обернуть в debounce.
    * @param {number} timeoutMs - Время задержки в миллисекундах.
    * @returns {Function} - Обернутая функция с задержкой.
    */
   debounce(callee, timeoutMs) {
-    return function perform(...args) {
-      let previousCall = this.lastCall;
-
+    return (...args) => {
+      const previousCall = this.lastCall;
       this.lastCall = Date.now();
 
       if (previousCall && this.lastCall - previousCall <= timeoutMs) {
