994530eb50212bfb62349d5f277f23442c9677f9
Igor Storchevoy <hellsgor@gmail.com>
2024-11-18 22:12:14 +0400

    Update[modalFromModal]: updated for forgot modal

--- src/js/services/modalFromModal.js
+++ src/js/services/modalFromModal.js
@@ -2,10 +2,20 @@ import { initializedModals, Modal } from '../components/modal';
 import { thankYou } from '../components/thank-you';
 
 export function modalFromModal(data, dataModalNameAttrValue) {
-  const entranceModal = initializedModals.find(
-    (modalInstance) => modalInstance.$modal.getAttribute(Modal.attrs.modalName) === dataModalNameAttrValue,
-  );
+  const modal = getModalInstanceByModalName(dataModalNameAttrValue);
 
-  entranceModal.hide(true);
-  thankYou(data);
+  if (modal.$modal.getAttribute(Modal.attrs.modalName) === 'forgot') {
+    getModalInstanceByModalName('entrance').setState(Modal.states.initialized);
+    modal.hide(true);
+  }
+
+  setTimeout(() => {
+    thankYou(data);
+  }, 500);
+}
+
+function getModalInstanceByModalName(modalName) {
+  return initializedModals.find(
+    (modalInstance) => modalInstance.$modal.getAttribute(Modal.attrs.modalName) === modalName,
+  );
 }
