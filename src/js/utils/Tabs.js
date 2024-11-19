188a82d9cf36fa9a65b4ecb32091b09ab67fcaca
Igor Storchevoy <hellsgor@gmail.com>
2024-11-19 14:10:21 +0400

    Docs[Tabs]: added documentation

--- src/js/utils/Tabs.js
+++ src/js/utils/Tabs.js
@@ -1,32 +1,71 @@
+/**
+ * Класс для реализации функционала вкладок (Tabs) на веб-странице.
+ * Позволяет переключать вкладки и отображать соответствующий контент.
+ */
 class Tabs {
-  $parentBlock = null;
-
-  tabs = [];
-  tabContentBlocks = [];
-
-  activeIndex = null;
-
+  /**
+   * Статический объект, содержащий атрибуты для поиска элементов вкладок и блоков контента.
+   * @static
+   * @type {Object}
+   * @property {string} tab Атрибут для выбора элементов вкладок.
+   * @property {string} tabContent Атрибут для выбора элементов контента вкладки.
+   * @property {string} tabParent Атрибут для выбора родительского элемента с вкладками.
+   */
   static attrs = {
     tab: 'data-tab',
     tabContent: 'data-tab-content',
     tabParent: 'data-tab-parent',
   };
 
+  /**
+   * Создаёт экземпляр класса и инициализирует вкладки и блоки контента.
+   * @param {HTMLElement} $parentBlock Родительский блок, содержащий вкладки и контент.
+   */
   constructor($parentBlock) {
+    /**
+     * Родительский элемент, в котором находятся вкладки и блоки контента.
+     * @type {HTMLElement}
+     */
     this.$parentBlock = $parentBlock;
 
+    /**
+     * Список элементов вкладок.
+     * @type {NodeListOf<HTMLElement>}
+     */
     this.tabs = this.getItems(Tabs.attrs.tab);
+
+    /**
+     * Список блоков контента для каждой вкладки.
+     * @type {NodeListOf<HTMLElement>}
+     */
     this.tabContentBlocks = this.getItems(Tabs.attrs.tabContent);
+
+    /**
+     * Индекс активной вкладки.
+     * @type {number|null}
+     */
+    this.activeIndex = null;
+
+    // Если вкладки и блоки контента существуют, инициализируем их.
     if (!this.tabs.length || !this.tabContentBlocks.length) return;
 
     this.prepare();
     this.addEvents();
   }
 
+  /**
+   * Получает элементы, соответствующие заданному атрибуту.
+   * @param {string} dataAttr Атрибут, по которому нужно искать элементы.
+   * @returns {NodeListOf<HTMLElement>} Список элементов, соответствующих атрибуту.
+   */
   getItems(dataAttr) {
     return this.$parentBlock.querySelectorAll(`[${dataAttr}]`);
   }
 
+  /**
+   * Подготавливает вкладки и устанавливает активную вкладку по умолчанию.
+   * Если в URL есть хеш, активируется соответствующая вкладка.
+   */
   prepare() {
     if (window.location.hash?.slice(1)) {
       this.activeIndex = window.location.hash
@@ -44,12 +83,19 @@ class Tabs {
     this.showTabContentBlock();
   }
 
+  /**
+   * Добавляет обработчики событий на клик по вкладкам.
+   */
   addEvents() {
     this.tabs.forEach(($tab) => {
       $tab.addEventListener('click', this.switch.bind(this));
     });
   }
 
+  /**
+   * Переключает вкладки при клике на них.
+   * @param {Event} event Событие клика по вкладке.
+   */
   switch(event) {
     const { currentTarget } = event;
 
@@ -58,10 +104,18 @@ class Tabs {
     this.showTabContentBlock();
   }
 
+  /**
+   * Устанавливает хеш в URL, соответствующий выбранной вкладке.
+   * @param {HTMLElement} target Элемент вкладки, на которую был клик.
+   */
   setHash(target) {
     window.location.hash = target.getAttribute(`${Tabs.attrs.tab}`);
   }
 
+  /**
+   * Активирует вкладку и деактивирует остальные.
+   * @param {HTMLElement} target Элемент вкладки, которая должна стать активной.
+   */
   activateTab(target) {
     this.tabs.forEach(($tab, idx) => {
       if ($tab === target) {
@@ -73,6 +127,9 @@ class Tabs {
     });
   }
 
+  /**
+   * Показывает или скрывает блоки контента в зависимости от активной вкладки.
+   */
   showTabContentBlock() {
     this.tabContentBlocks.forEach(($block) =>
       $block.classList[
@@ -87,6 +144,9 @@ class Tabs {
   }
 }
 
+/**
+ * Инициализирует вкладки на всех элементах с атрибутом data-tab-parent.
+ */
 export function initTabs() {
   document.querySelectorAll(`[${Tabs.attrs.tabParent}]`).forEach(($tabsParent) => {
     new Tabs($tabsParent);
