80c17824ec3ed5a2cce5e22d4c6777ec9ff27645
Igor Storchevoy <hellsgor@gmail.com>
2024-11-19 13:55:24 +0400

    Docs[Swiper]: added documentation

--- src/js/services/Swiper.js
+++ src/js/services/Swiper.js
@@ -2,13 +2,31 @@ import Swiper from 'swiper';
 import 'swiper/css';
 import { Navigation, Pagination } from 'swiper/modules';
 
+/**
+ * Класс для инициализации и управления слайдерами.
+ * Использует библиотеку Swiper для создания слайдеров с возможностью настройки и управления их поведением.
+ */
 export class InitSlider {
+  /** @type {string} CSS-класс для слайдера. */
   classSlider = '';
+  /** @type {HTMLElement|null} Родительский элемент слайдера. */
   classSliderParent = null;
+  /** @type {string|null} CSS-класс для элементов заголовков слайдов. */
   classSliderTitles = null;
+  /** @type {Object} Настройки слайдера, передаваемые при инициализации. */
   settingsSlider = {};
+  /** @type {Swiper|null} Экземпляр слайдера Swiper. */
   slider = null;
 
+  /**
+   * Конструктор класса InitSlider.
+   *
+   * @param {Object} props - Объект с параметрами для слайдера.
+   * @param {string} props.classSlider - CSS-класс слайдера.
+   * @param {string|null} [props.classSliderParent=null] - CSS-класс родительского элемента слайдера.
+   * @param {string|null} [props.classSliderTitles=null] - CSS-класс для заголовков слайдов.
+   * @param {Object} props.settingsSlider - Объект с настройками слайдера (для Swiper).
+   */
   constructor(props) {
     this.classSlider = props.classSlider;
     this.classSliderParent = props.classSliderParent || null;
@@ -24,6 +42,10 @@ export class InitSlider {
     this.initTitles();
   }
 
+  /**
+   * Проверяет, нужно ли инициализировать или уничтожить слайдер в зависимости от размеров экрана.
+   * Если размер экрана соответствует критерию destroySize, слайдер будет уничтожен.
+   */
   checkSlider() {
     if (window.matchMedia(this.settingsSlider.destroySize).matches && this.settingsSlider.destroySize) {
       if (this.slider) {
@@ -41,12 +63,18 @@ export class InitSlider {
     }
   }
 
+  /**
+   * Добавляет обработчик события изменения размера окна для проверки и повторной инициализации слайдера.
+   */
   checkResizeSlider() {
     window.addEventListener('resize', () => {
       this.checkSlider();
     });
   }
 
+  /**
+   * Инициализирует слайдер с использованием библиотеки Swiper.
+   */
   initSlider() {
     this.slider = new Swiper(this.classSlider, this.settingsSlider) || null;
 
@@ -59,6 +87,10 @@ export class InitSlider {
     }
   }
 
+  /**
+   * Обновляет состояние пагинации слайдера.
+   * Применяет активный класс к текущему элементу пагинации.
+   */
   updatePagination() {
     const bullets = document.querySelectorAll(
       `${this.settingsSlider.pagination.el} .${this.settingsSlider.pagination.bulletClass}`,
@@ -74,6 +106,9 @@ export class InitSlider {
     });
   }
 
+  /**
+   * Уничтожает слайдер и очищает все его стили.
+   */
   destroySlider() {
     this.slider.destroy();
     this.slider = null;
@@ -83,6 +118,9 @@ export class InitSlider {
     document.querySelector(`${this.classSlider}__wrapper`)?.removeAttribute('style');
   }
 
+  /**
+   * Инициализирует обработчики для заголовков слайдов, позволяя переключать слайды по клику на заголовок.
+   */
   initTitles() {
     if (!this.classSliderParent || !this.classSliderTitles || !this.settingsSlider.pagination.bulletClass) return;
 
@@ -106,6 +144,11 @@ export class InitSlider {
   }
 }
 
+/**
+ * Массив с объектами, содержащими параметры и настройки для различных слайдеров.
+ *
+ * @type {Array<Object>}
+ */
 const listSliders = [
   {
     classSlider: '.four-steps-slider',
@@ -201,6 +244,11 @@ const listSliders = [
   },
 ];
 
+/**
+ * Инициализирует все слайдеры, используя параметры из массива listSliders.
+ *
+ * @returns {Array<InitSlider>} Массив экземпляров класса InitSlider.
+ */
 export const initSwipers = () => {
   return listSliders.map((i) => {
     return new InitSlider(i);
