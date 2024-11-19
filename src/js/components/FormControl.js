9514acc5dfed9b32a18007eb090700c889c0661d
Igor Storchevoy <hellsgor@gmail.com>
2024-11-19 13:31:09 +0400

    Docs[FormControl]: added documentation

--- src/js/components/FormControl.js
+++ src/js/components/FormControl.js
@@ -1,10 +1,23 @@
 import gsap from 'gsap';
 
+/**
+ * Класс `FormControl` отвечает за обработку состояния и анимацию ошибок для полей формы.
+ */
 class FormControl {
+  /**
+   * @property {HTMLInputElement} $input - Входное поле формы.
+   * @property {HTMLElement} $control - Контейнер, содержащий элемент управления формой.
+   * @property {HTMLElement} $error - Элемент для отображения текста ошибки.
+   */
+
   $input = null;
   $control = null;
   $error = null;
 
+  /**
+   * Создаёт экземпляр `FormControl`.
+   * @param {HTMLInputElement} $input - Элемент input, для которого будет обрабатываться состояние ошибки.
+   */
   constructor($input) {
     this.$input = $input;
     this.$control = $input.closest('div');
@@ -13,10 +26,19 @@ class FormControl {
     this.addEvents();
   }
 
+  /**
+   * Добавляет обработчики событий на элемент input.
+   * В частности, добавляется обработчик `focus`, который скрывает сообщение об ошибке при фокусе.
+   * @private
+   */
   addEvents() {
     this.$input.addEventListener('focus', this.hideError.bind(this));
   }
 
+  /**
+   * Скрывает сообщение об ошибке с анимацией.
+   * Если текст ошибки отсутствует, функция завершает выполнение.
+   */
   hideError() {
     if (!this.$error?.textContent) return;
 
@@ -26,7 +48,6 @@ class FormControl {
       },
       opacity: 0,
       duration: 0.3,
-
       onComplete: () => {
         this.$error.textContent = '';
         this.$error.removeAttribute('style');
@@ -35,6 +56,10 @@ class FormControl {
   }
 }
 
+/**
+ * Инициализирует элементы управления формы.
+ * Находит все input-элементы на странице и создаёт экземпляры `FormControl` для каждого из них.
+ */
 export function initControls() {
   document.querySelectorAll('input').forEach(($control) => new FormControl($control));
 }
